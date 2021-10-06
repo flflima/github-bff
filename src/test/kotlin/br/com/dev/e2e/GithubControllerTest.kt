05d4782abb42c659f64a46a46ce10d072b056dcf
Felipe Lima <flf.felipe@gmail.com>
2021-10-06 19:46:01 -0300

    test: first e2e test

--- src/test/kotlin/br/com/dev/e2e/GithubControllerTest.kt
+++ src/test/kotlin/br/com/dev/e2e/GithubControllerTest.kt
@@ -1,7 +1,24 @@
 package br.com.dev.e2e
 
+import io.micronaut.context.ApplicationContext
+import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import io.restassured.RestAssured
+import org.hamcrest.CoreMatchers
+import org.junit.jupiter.api.Test
 
 @MicronautTest
 class GithubControllerTest {
+
+    private val embeddedServer: EmbeddedServer = ApplicationContext.run(EmbeddedServer::class.java)
+
+    @Test
+    fun `it should return the username in the path URL`() {
+        RestAssured.given().port(embeddedServer.port)
+            .`when`()
+            .get("/github/user-test/timeline")
+            .then()
+            .statusCode(200)
+            .body(CoreMatchers.equalTo("user-test"))
+    }
 }
